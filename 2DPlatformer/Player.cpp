1468cf1eda8d0253491c5b4cd964b6d442e8607f
Starkiller2000 <Austen.Oberheu@me.com>
2018-02-21 01:04:04 -0500

    Added animations
    Added animations

--- 2DPlatformer/Player.cpp
+++ 2DPlatformer/Player.cpp
@@ -2,13 +2,11 @@
 #include "Player.h"
 
 
-Player::Player(sf::Sprite &playerShape, sf::Texture &playerTexture)
+Player::Player(sf::Sprite &playerShape)
 {
-	
-	playerShape.setPosition(0, 300);
-	playerShape.setOrigin(12.5, 25.f);
+	playerShape.setPosition(25, 300);
 	playerShape.setScale(.2f, .2f);
-	playerShape.setTexture(playerTexture);
+	
 	velocity.x = 0; 
 	velocity.y = 0;
 	lastPosition.x = playerShape.getPosition().x;
@@ -16,9 +14,10 @@ Player::Player(sf::Sprite &playerShape, sf::Texture &playerTexture)
 	maxHorizontalVelocity = 500;
 	minHorizontalVelocity = -500;
 	maxVerticalVelocity = 1000;
-	minVerticalVelocity = -500;
+	minVerticalVelocity = -1500;
 	allowedToJump = false;
-
+	
+	time = 0.f;
 
 }
 
@@ -31,26 +30,33 @@ void Player::Update(sf::Sprite &playerShape, float deltaTime, std::vector<sf::Fl
 {
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 	{
+		
+		playerShape.setScale(.2f, .2f);
 		if (velocity.x < maxHorizontalVelocity)
 		{
 			velocity.x += 50;
 			std::cout << velocity.x << std::endl;
 		}
+		walking = true;
 
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 	{
+		
+		playerShape.setScale(-.2f, .2f);
+
 		if (velocity.x > minHorizontalVelocity)
 		{
 			velocity.x -= 50;
 			
 		}
+		walking = true;
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
 	{
 		if (allowedToJump == true)
 		{
-			velocity.y -= 500;
+			velocity.y -= 1500;
 			allowedToJump = false;
 			jumping = true;
 			falling = true;
@@ -73,6 +79,7 @@ void Player::Update(sf::Sprite &playerShape, float deltaTime, std::vector<sf::Fl
 	}
 	if (!sf::Keyboard::isKeyPressed(sf::Keyboard::A) && (!sf::Keyboard::isKeyPressed(sf::Keyboard::D)))
 	{
+		walking = false;
 		if (velocity.x < 0)
 		{
 			velocity.x += 25;
@@ -85,40 +92,89 @@ void Player::Update(sf::Sprite &playerShape, float deltaTime, std::vector<sf::Fl
 
 	if (falling == true)
 	{
-		velocity.y += 10;
+		velocity.y += 50;
 	}
 
 	sf::FloatRect playerBoundingBox = playerShape.getGlobalBounds();
+	
 
 	for (int i = 0; i < blockBoundingBox.size(); i++)
 	{
 		if (playerBoundingBox.intersects(blockBoundingBox[i]))
 		{
-			falling = false;
-			if (velocity.y > 0 || velocity.y < 0)
+			
+			
+			if (velocity.y > 0 && allowedToJump == false /*|| velocity.y < 0*/)
 			{
+				falling = false;
 				allowedToJump = true;
 				velocity.y = 0;
-				playerShape.setPosition(lastPosition);
+				playerShape.setPosition(playerShape.getPosition().x, lastPosition.y);
 				break;
 			}
 		}
 	}
 
 	//std::cout << "x: " << velocity.x << " y: " << velocity.y << std::endl;
-	lastPosition.x = playerShape.getPosition().x;
+	//lastPosition.x = playerShape.getPosition().x;
 	lastPosition.y = playerShape.getPosition().y;
 
 	playerShape.move(velocity.x * deltaTime, velocity.y * deltaTime);
 
 }
 
-sf::Sprite Player::playerSpriteUpdate(sf::Sprite &playerSprite, std::vector<sf::Texture>& spriteTexture, float deltaTime)
+sf::Sprite Player::playerSpriteUpdate(sf::Sprite &playerSprite, std::vector<sf::Texture>& idleAnimation, std::vector<sf::Texture>& walkingAnimation, float deltaTime, sf::Vector2f origin)
 {
-	/*if deltaTime*/
-	playerSprite.setTexture(spriteTexture[frame]);
-	frame += 1;
-	if (frame == 9)
-		frame = 0;
+	time += deltaTime;
+
+	if ((time / deltaTime) > 10) 
+	{
+		if (walking == true)
+		{
+			
+			playerSprite.setTexture(walkingAnimation[frame]);
+			frame += 1;
+			if (frame == 9)
+				frame = 0;
+			time = 0.f;
+			
+			playerSprite.setTextureRect(sf::IntRect(origin.x, origin.y, walkingAnimation[frame].getSize().x, walkingAnimation[frame].getSize().y));
+		}
+		else
+		{
+			playerSprite.setTexture(idleAnimation[frame]);
+			frame += 1;
+			if (frame == 9)
+				frame = 0;
+			time = 0.f;
+			playerSprite.setTextureRect(sf::IntRect(origin.x, origin.y, idleAnimation[frame].getSize().x, idleAnimation[frame].getSize().y));
+		}
+		
+	}
 	return playerSprite;
+}
+
+void Player::textureInit(std::vector<sf::Texture> &idleAnimation, std::vector <sf::Texture> &walkingAnimation)
+{
+	idleAnimation[0].loadFromFile("Textures/Idle__000.png");
+	idleAnimation[1].loadFromFile("Textures/Idle__001.png");
+	idleAnimation[2].loadFromFile("Textures/Idle__002.png");
+	idleAnimation[3].loadFromFile("Textures/Idle__003.png");
+	idleAnimation[4].loadFromFile("Textures/Idle__004.png");
+	idleAnimation[5].loadFromFile("Textures/Idle__005.png");
+	idleAnimation[6].loadFromFile("Textures/Idle__006.png");
+	idleAnimation[7].loadFromFile("Textures/Idle__007.png");
+	idleAnimation[8].loadFromFile("Textures/Idle__008.png");
+	idleAnimation[9].loadFromFile("Textures/Idle__009.png");
+
+	walkingAnimation[0].loadFromFile("Textures/Run__000.png");
+	walkingAnimation[1].loadFromFile("Textures/Run__001.png");
+	walkingAnimation[2].loadFromFile("Textures/Run__002.png");
+	walkingAnimation[3].loadFromFile("Textures/Run__003.png");
+	walkingAnimation[4].loadFromFile("Textures/Run__004.png");
+	walkingAnimation[5].loadFromFile("Textures/Run__005.png");
+	walkingAnimation[6].loadFromFile("Textures/Run__006.png");
+	walkingAnimation[7].loadFromFile("Textures/Run__007.png");
+	walkingAnimation[8].loadFromFile("Textures/Run__008.png");
+	walkingAnimation[9].loadFromFile("Textures/Run__009.png");
 }
--- 2DPlatformer/Player.h
+++ 2DPlatformer/Player.h
@@ -1,26 +1,39 @@
 //#pragma once
 #include <iostream>
+#include <string>
+#include <vector>
 #include <SFML/Graphics.hpp>
 class Player
 {
 public:
-	Player(sf::Sprite &playerShape, sf::Texture &playerTexture);
+	Player(sf::Sprite &playerShape);
 	~Player();
 
 	void Update(sf::Sprite &playerShape, float deltaTime, std::vector<sf::FloatRect> &blockBoundingBox);
 
-	sf::Sprite playerSpriteUpdate(sf::Sprite &playerSprite, std::vector<sf::Texture> &spriteTexture, float deltaTime);
+	sf::Sprite playerSpriteUpdate(sf::Sprite &playerSprite, std::vector<sf::Texture> &spriteTexture, std::vector<sf::Texture>& walkingAnimation, float deltaTime, sf::Vector2f origin);
+
+	void textureInit(std::vector<sf::Texture> &idleAnimation, std::vector <sf::Texture> &walkingAnimation);
 
 public:
+	
+
 	sf::Vector2f lastPosition;
 	sf::Vector2i velocity;
+	sf::Time animationTimer;
+
+	float time;
+
+	bool walking = true;
 	bool jumping = false;
 	bool falling = false;
 	bool allowedToJump;	
+
 	int maxHorizontalVelocity;
 	int minHorizontalVelocity;
 	int maxVerticalVelocity;
 	int minVerticalVelocity;
 	int frame = 0;
+
 };
 
